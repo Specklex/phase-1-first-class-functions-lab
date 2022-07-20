08206adb31e82409f9db7673751506231101199e
Jaden Lund <lundjaden@gmail.com>
2022-07-19 22:17:03 -0500

    commit tree 2ec78e7446cf72e707e7e456d99b2f37692a175c

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function (drivers) { return drivers.slice(0, 2) };
+
+const returnLastTwoDrivers = function (drivers) { return drivers.slice(drivers.length - 2, drivers.length) }
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+//allows us to invoke either function from the array
+
+function createFareMultiplier(multiplier) {
+    return function (farePrice) {
+        return multiplier * farePrice;
+    }
+    //Help from: 
+    //https://www.linkedin.com/pulse/quick-view-higher-order-functions-elie-gakuba
+}
+
+function fareDoubler(farePrice) {return (farePrice * 2)}
+
+function fareTripler(farePrice) {return (farePrice * 3)}
+
+function selectDifferentDrivers(drivers, selectingDrivers) {
+    return selectingDrivers(drivers)
+}
